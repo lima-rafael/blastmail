b591c198f288d8a5535d8ec67ca72a64812707e8
Rafael <rafa.lima.01@live.com>
2026-01-02 16:01:28 -0300

    feat: Laravel is installed and configured in my project.

---
+++ app/Http/Controllers/EmailListController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEmailListRequest;
+use App\Http\Requests\UpdateEmailListRequest;
+use App\Models\EmailList;
+
+class EmailListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEmailListRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EmailList $emailList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EmailList $emailList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEmailListRequest $request, EmailList $emailList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EmailList $emailList)
+    {
+        //
+    }
+}
