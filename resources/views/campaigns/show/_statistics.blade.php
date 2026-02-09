da679b4cdf9cbb367b3c85b73ba77943217822a3
Rafael <rafa.lima.01@live.com>
2026-02-08 23:22:05 -0300

    feat: Scream statistics

--- resources/views/campaigns/show/_statistics.blade.php
+++ resources/views/campaigns/show/_statistics.blade.php
@@ -1,3 +1,11 @@
-<x-card>
-    Statistics
-</x-card>
+<div class="flex flex-col gap-4">
+    <x-alert success noIcon :title="__('Your campaign is ready to be send!')" />
+    <div class="grid grid-cols-3 gap-5">
+        <x-dashboard.card heading="01" subheading="{{ __('Opens') }}" />
+        <x-dashboard.card heading="02" subheading="{{ __('Unique Opens') }}" />
+        <x-dashboard.card heading="20%" subheading="{{ __('Opens rate') }}" />
+        <x-dashboard.card heading="0" subheading="{{ __('Clicks') }}" />
+        <x-dashboard.card heading="0" subheading="{{ __('Unique Clicks') }}" />
+        <x-dashboard.card heading="20%" subheading="{{ __('Clicks Rate') }}" />
+    </div>
+</div>
--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -4,6 +4,7 @@
     'info' => null,
     'warning' => null,
     'danger' => null,
+    'noIcon' => false,
 ])
 
 <div @class([
@@ -28,12 +29,14 @@
             'text-amber-500 bg-sky-500/15' => $warning,
             'text-red-500 bg-sky-500/15' => $danger,
         ]) aria-hidden="true">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-6"
-                aria-hidden="true">
-                <path fill-rule="evenodd"
-                    d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
-                    clip-rule="evenodd" />
-            </svg>
+            @unless ($noIcon)
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-6"
+                    aria-hidden="true">
+                    <path fill-rule="evenodd"
+                        d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+                        clip-rule="evenodd" />
+                </svg>
+            @endunless
         </div>
         <div class="ml-2">
             <h3 @class([
