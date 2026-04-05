d29b3bb7a3ac0c20fee223acc09f0dac68095523
Rafael <rafa.lima.01@live.com>
2026-04-05 19:17:56 -0300

    feat: Create test for View

--- tests/Feature/Templates/DeleteTest.php
+++ tests/Feature/Templates/DeleteTest.php
@@ -9,9 +9,7 @@ pest()->group('templatesDelete');
 it('should be able to delete a template from a list', function () {
     login();
     $template = Template::factory()->create();
-
-    // dd($template, $template->email);
-
+    
     delete(route('template.destroy', ['template' => $template]))
         ->assertRedirectToRoute('template.index')
         ->assertSessionHas('message', __('Template deleted succcessfully.'));
